e02aa5017feb0de50863c28faf1ffbdbc86250ff
bluepaun <bluepaun@gmail.com>
2022-10-04 00:55:37 +0800

    babel and create image test

---
+++ src/generator/image.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import { loadImage, createCanvas } from "canvas";
+const width = 1200;
+const height = 650;
+const canvas = createCanvas(width, height);
+const context = canvas.getContext("2d");
+context.fillStyle = "#2b03a3";
+context.fillRect(0, 0, width, height);
+context.font = "bold 72pt Menlo";
+context.textBaseline = "top";
+context.textAlign = "center";
+context.fillStyle = "#f7ab07";
+const imgText = "Tiny Text on Canvas";
+const textAlign = context.measureText(imgText).width;
+// context.fillRect(590 - textAlign / 2 - 10, 170 - 5, textAlign + 20, 120);
+// context.fillStyle = "#ffffff";
+context.fillText(imgText, 555, 120);
+// context.fillStyle = "#ffffff";
+// context.font = "bold 32pt Menlo";
+// context.fillText("positronx.io", 755, 600);
+
+const imgBuffer = canvas.toBuffer("image/png");
+fs.writeFileSync("./resources/drawnImage.png", imgBuffer);
+// loadImage("./img/samurai.jpg").then((data) => {
+//     context.drawImage(data, 340, 515, 70, 70);
+//     const imgBuffer = canvas.toBuffer("image/png");
+//     fs.writeFileSync("./resources/drawnImage.png", imgBuffer);
+// });
